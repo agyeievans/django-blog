b7aca8d44405ea962106c119da274ff6a836e0d5
agyeievans <evanskofiagyei06@gmail.com>
2023-05-04 15:48:53 +0000

    retrieve data from database

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
