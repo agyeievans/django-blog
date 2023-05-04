89b9b93856b25e86c812ace0c8823a86d452d40d
agyeievans <evanskofiagyei06@gmail.com>
2023-05-04 16:03:34 +0000

    added post

--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'posts.html', {'posts': posts})
